bf16a231a8338436d6a1161cdd6c25273456d2fd
Patricio <patricioescabe@gmail.com>
2025-05-27 16:10:26 +0200

    last try

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3001',
+  apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:10000',
   appEnv: import.meta.env.VITE_APP_ENV || 'development'
 };
 
